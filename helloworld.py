93f701975f0bc74369d750e3877bdb988c8712f4
herewo <50653064+herewo@users.noreply.github.com>
2022-03-20 01:27:13 +0800

    Update helloworld.py

--- helloworld.py
+++ helloworld.py
@@ -1,7 +1,9 @@
 def test_create(name,msg):
-  desktop_path = 'D:\Jenkins\workspace'
+  desktop_path = "D:\JenkinsTest"
   full_path = desktop_path + name + ".txt"
   file = open(full_path,'w')
   file.write(msg)
   file.close()
+  print("OK!!!!!!")
+
 test_create("hello","hello world")
